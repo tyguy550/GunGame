142b648fc03e895c0b30ba65f36c4c3abaf9eef5
tyguy550 <alexjohnyoung@gmail.com>
2013-07-16 02:16:41 +0100

    Fixed something

--- gamemode/settings.lua
+++ gamemode/settings.lua
@@ -2,10 +2,6 @@ GunGame.RoundDownBy = 30 --How much the round wait should go down by every two s
 GunGame.DeathGunBack = true --Enable/disable this so if a player dies they go back by one 
 GunGame.ZedTimeEnabled = true --Enable/disable ZedTime with this variable
 GunGame.ZedTimeSpeed = 0.1 --This is the speed that you change to when zedtime is active, change this variable to modify the speed (normal speed being 1)
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 2316698dac40fa6c3cc795e5ac04d74cb01bc269
 GunGame.StopSuiciding = true --Enable/disable this to stop/allow players to suicide (people can suicide and the attacker won't proceed to the next weapon)
 GunGame.FirstSpawnF1Menu = true --Enable/disable this to make/unmake the F1 Menu open on first spawn (when a player joins)
 
